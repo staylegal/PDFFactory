7802c20f00c30b05364f5fb15e9373cb97577be1
Steve Johns <steve.johns@dds.co>
2012-05-15 15:40:34 +0100

    updated config file comments

--- PDFFactory/config.php
+++ PDFFactory/config.php
@@ -1,18 +1,43 @@
 <?php
 
-// set path to wkhtmltopdf executionable i.e. /usr/bin for unix, C:\Program Files\wkhtmltopdf for windows
+/**
+ * PDFFactory config file
+ */
+
+$config = array();
+
+/**
+ * Set path to wkhtmltopdf executionable
+ * i.e. /usr/bin for Unix, C:\Program Files\wkhtmltopdf for Windows
+ * @var string
+ */
 $config['wkhtmltopdf_directory'] = '/usr/bin';
 
-// set path to ghostscript executionable i.e. /usr/bin for unix, C:\Program Files\GSLITE for windows
+/**
+ * Set path to Ghostscript executionable
+ * i.e. /usr/bin for Unix, C:\Program Files\GSLITE for Windows
+ * @var string
+ */
 $config['ghostscript_directory'] = '/usr/bin';
 
-// set the default ouput directory for any generated PDFs
+/**
+ * Set the default output directory for generated PDFs
+ * @var string
+ */
 $config['pdf_output_directory'] = '/home/t/e/testpyramid/web/public_html/output/';
 
-// set the wkhtmltopdf executionable name i.e. wkhtmltopdf for unix, wkhtmltopdf.exe for windows
+/**
+ * Set the wkhtmltopdf executionable name
+ * i.e. wkhtmltopdf for Unix or wkhtmltopdf.exe for Windows
+ * @var string
+ */
 $config['wkhtmltopdf_executionable'] = 'wkhtmltopdf';
 
-// set the ghostscript executionable name i.e. ghostscript for unix, gswin32c.exe for windows
+/**
+ * Set the Ghostscript executionable name
+ * i.e. gs for Unix or gswin32c.exe for windows
+ * @var string
+ */
 $config['ghostscript_executionable'] = 'gs';
 
 ?>
